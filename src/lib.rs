68798e856cb8b977db09f9b6f4667cb83cfe0861
Timothy Carambat <rambat1010@gmail.com>
2026-08-28 10:19:02 -0700

    update test fix and edge cases

--- src/lib.rs
+++ src/lib.rs
@@ -255,7 +255,7 @@ async fn markdown_to_typst_async(
                 //         "#link(label(\"{}\"))[{}]", current_link_url.replace("#", ""), current_link_text
                 //     ))
                 // } else {
-                let result = &format!("#link(\"{current_link_url}\")[{current_link_text}]");
+                let result = &format!("#link(\"{current_link_url}\")[{current_link_text}];");
                 // Get the current output buffer based on context
                 let current_output: &mut String = if in_code_block {
                     &mut current_code_block
@@ -2153,7 +2153,7 @@ This is a new paragraph with <u>underlined text."#;
 
         let (typst_code, _) = result.unwrap();
 
-        assert!(typst_code.contains(r#"+ Third item with #link("http://test.com")[link with \*\*bold inside] and \*italic not closed"#));
+        assert!(typst_code.contains(r#"+ Third item with #link("http://test.com")[link with \*\*bold inside]; and \*italic not closed"#));
         assert!(typst_code.contains(
             "+ Second item with #strong[markdown bold and #emph[HTML em]] but em not closed"
         ));
